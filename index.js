64b88c8a56eced328571fcd1b738c43716f3907d
Sahej Jain <89766122+Greninja28@users.noreply.github.com>
2022-08-17 23:24:55 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const socket = io('http://localhost:8000', { transports: ['websocket', 'polling', 'flashsocket'] });
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('Message Tone.mp3');
+
+const append = (message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const name = prompt("Enter your name: ");
+socket.emit('new-user-joined', name);
+socket.on('user-joined', name=>{
+    append(`${name} joined the chat`, `left`);
+})
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`, `left`);
+}) 
+socket.on('left', name=>{
+    append(`${name} left the chat`, `right`);
+})
